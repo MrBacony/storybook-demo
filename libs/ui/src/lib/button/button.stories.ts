1788714a4f985ee401abb304ed4d377815ba58ac
Gregor Speck <gregor.speck@bridging-it.de>
2020-04-23 22:40:57 +0200

    reverted to add knobs

--- libs/ui/src/lib/button/button.stories.ts
+++ libs/ui/src/lib/button/button.stories.ts
@@ -1,19 +1,9 @@
 import {ButtonComponent} from "./button.component";
 import {text, withKnobs} from "@storybook/addon-knobs";
-import figmaDecorator from 'storybook-addon-figma';
-import {withDesign} from "storybook-addon-designs";
+
 export default {
   title: 'Special Button',
-  decorators: [
-    withKnobs,
-    withDesign
-  ],
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/cvCrCmpy90x42ulcfiP27V/Untitled?node-id=2%3A2'
-    }
-  }
+  decorators: [withKnobs]
 }
 
 export const Default = () => ({
