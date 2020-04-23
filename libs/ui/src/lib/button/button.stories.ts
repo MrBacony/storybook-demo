921f28f2fcc649c905136a2739de54189ab3f1f5
Gregor Speck <gregor.speck@bridging-it.de>
2020-04-23 22:08:27 +0200

    added knobs

--- libs/ui/src/lib/button/button.stories.ts
+++ libs/ui/src/lib/button/button.stories.ts
@@ -1,12 +1,28 @@
 import {ButtonComponent} from "./button.component";
+import {text, withKnobs} from "@storybook/addon-knobs";
 
 export default {
-  title: 'Special Button'
+  title: 'Special Button',
+  decorators: [withKnobs]
 }
 
 export const Default = () => ({
   component: ButtonComponent,
   props: {
-    label: 'Button Label'
+    label: text('label', 'Button Label')
+  }
+});
+
+export const FlatButton = () => ({
+  component: ButtonComponent,
+  props: {
+    label: text('label','Flat Button'),
+  }
+});
+
+export const OutlineButton = () => ({
+  component: ButtonComponent,
+  props: {
+    label: text('label','Outline Button')
   }
 });
