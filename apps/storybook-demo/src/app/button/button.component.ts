4fb84c9c2a975f5395d8178ac0e145e2808c0a56
Gregor Speck <gregor.speck@bridging-it.de>
2020-04-23 21:31:52 +0200

    added lib and base styling

---
+++ apps/storybook-demo/src/app/button/button.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'storybook-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input()
+  label: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
